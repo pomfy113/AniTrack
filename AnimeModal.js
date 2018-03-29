d75344c620ccbdcd570e44b71f4fee41c11ce145
pomfy113 <pomfy113@gmail.com>
2018-03-28 18:01:03 -0700

    We have SOMETHING in our modal

--- AnimeModal.js
+++ AnimeModal.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Button, TouchableOpacity, Modal, View, Text} from 'react-native';
+import { StyleSheet, Button, TouchableOpacity, Image, Modal, View, Text} from 'react-native';
 
 
 export default class AnimeModal extends Component{
@@ -12,7 +12,15 @@ export default class AnimeModal extends Component{
                 transparent={true}>
                 <View style={styles.modalBG}>
                     <View style={styles.modalBox}>
-                        <Text>{this.props.anime.title}</Text>
+                        <View style={styles.miniBox}>
+                            <Image style={{width: 112, height: 146}} source={{uri: this.props.anime.picture}}/>
+                            <View style={{marginLeft: 30}}>
+                                <Text>{this.props.anime.title}</Text>
+                                <Text>{this.props.anime.score} / 10 </Text>
+                            </View>
+                        </View>
+
+                        <Text>{this.props.anime.synopsis}</Text>
                         <Button onPress={() => this.props.changeModal()} title="Close"/>
                     </View>
                 </View>
@@ -36,5 +44,8 @@ const styles = StyleSheet.create({
         width: "80%",
         backgroundColor: "white",
         justifyContent: "space-between"
+    },
+    miniBox: {
+        flexDirection: "row"
     }
 });
